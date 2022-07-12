7b40e5cdec9237f90a0c212edb25f4736c32b091
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 15:21:03 +0700

    Program Bahasa C Persegi

---
+++ Program persegi/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //Luas Persegi
+
+    int sisi,luas;
+
+    printf("\n Masukkan panjang sisi persegi : ");
+    scanf("%d",&sisi);
+
+    luas=sisi*sisi;
+
+    printf("\n Luas persegi adalah %d \n",luas);
+    return 0;
+}
