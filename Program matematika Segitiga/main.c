de742f81aa85c3d9447a24494cda41afbba39668
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 14:54:08 +0700

    Program Bahas C Matematika untuk membuat Segitiga

---
+++ Program matematika Segitiga/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luasSegitiga;
+    int segitigaAlas;
+    int tinggiSegitiga;
+
+
+    luasSegitiga=segitigaAlas*tinggiSegitiga;
+    printf("\nLuas Segitiga adalah : %d\n",luasSegitiga);
+    return 0;
+}
