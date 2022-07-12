bc7cfa98df69a35290741e74e881952c466e50a0
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 14:53:08 +0700

    Program Bahas C Inputan

---
+++ Program masukan f/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A,b=10,c;
+    float X,Y=10.5;
+    char ch1,ch2='a',str1[10],str2[10]="TelU";
+
+    printf("masukan Nilai F\n");
+    scanf("%f",&X);
+    Y=X+Y;
+    printf("X=%f Y=%f",X,Y);
+
+}
