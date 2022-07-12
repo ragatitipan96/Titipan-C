0e311c9fb51c7c5adb36fe6fecbadfb64a09671e
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 15:30:12 +0700

    Program Bahasa C Struck Rekaman

---
+++ Program Struct Rekaman/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct rekaman {
+ char nama[25];
+ int NIM;
+ int Nilai} Rek;
+
+int KelNIM, SumNil, NKuliah;
+float Rata;
+
+FILE *Arsip;
+
+void Bacadata(int N){
+    int i;
+    Arsip = fopen("Data.txt","w");
+    for (i=0;i<N;i++){
+    printf("Masukkan nim :"); scanf("%d",&Rek.NIM);
+    printf("Masukkan nilai:"); scanf("%d", &Rek.Nilai);
+    fwrite(&Rek, sizeof(Rek),1,Arsip);
+    }
+    fclose(Arsip);
+}
+
+void Hitungrata(int N){
+    Arsip=fopen("Data.txt","r");
+    fread(&Rek, sizeof(Rek),1,Arsip);
+    while (!feof(Arsip)){
+    SumNil = 0; NKuliah = 0;   // Nilai awal SumNil dan Nkuliah tiap NIM
+    KelNIM = Rek.NIM;         // Kunci diisi dengan NIM tiap kelompok
+    do {
+        SumNil = SumNil+Rek.Nilai;   // menghitung SUmNil dan NKuliah
+        NKuliah = NKuliah + 1;
+        fread(&Rek, sizeof(Rek),1,Arsip); // Baca data berikutnya
+    } while ( KelNIM == Rek.NIM && !feof(Arsip));  // kondisi while
+    printf("KelNIM=%d,SumNil=%d, NKuliah=%d, Rata2 nilai=%f \n",
+    KelNIM,SumNil,NKuliah,(float)SumNil/NKuliah);
+    KelNIM=Rek.NIM;    // Kunci diisi dengan NIM kelompok berikutnya
+    }
+    fclose(Arsip);
+}
+
+
+int main()
+{
+    int n;
+    printf("Jumlah data : ");
+    scanf("%d",&n);
+    Bacadata(n);
+    Hitungrata(n);
+    return 0;
+}
