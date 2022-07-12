f0872a6a6a7279f838b117a8ade59c9e4997f31a
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 13:18:45 +0700

    Program Biodata 2

---
+++ Program biodata 2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nomor;
+    char nama [20],alamat [40], kelas [10];
+    char jns_kel;
+    printf ("masukan nomor = ");
+    scanf("%d",&nomor);
+    fflush(stdin);
+    printf ("masukan nama = ");
+    fgets(nama,20,stdin);
+    fflush(stdin);
+    printf("masukan jns_kel = ");
+    scanf ("%c",&jns_kel);
+    fflush(stdin);
+    printf("masukan alamat = ");
+    fgets(alamat,40,stdin);
+    strcpy(kelas,"TK-43-04");
+    printf("nomor=%d nama=%s jns_kel=%c alamat=%s kelas=%s",nomor,nama,jns_kel,alamat,kelas);
+
+    return 0;
+}
