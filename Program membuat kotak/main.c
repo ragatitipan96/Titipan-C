c5fe56294506f1fe8048195143ce13eb22b83f5e
Titipan <73322909+ragatitipan96@users.noreply.github.com>
2022-07-12 14:55:15 +0700

    Program Bahas C Untuk membuat Kotak

---
+++ Program membuat kotak/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,j;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        if((i==0)||(i==n-1)||(j==0)||(j==-1)){
+            printf("*");
+        }
+    }else {
+        if(i%2==1){
+            printf("0");
+        }
+    }
+    if(a=0;i<5;i++){
+        printf("*");
+    }
+    return 0;
+}
